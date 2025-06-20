d94e14a1e9c9003da069fd356ebd3d79156ff3bb
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-06-20 06:52:40 -0500

    feat: add functions in main

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from utils.driver import get_driver
-from modules import RF_001  # Importar con guión bajo por convención de Python
+from modules import CP_RF001_001, CP_RF001_002, CP_RF001_003, CP_RF021_001, CP_RF021_003, CP_RF021_002
+from utils.common import login_como_instructor
 
 def main():
     driver = get_driver()
 
     try:
         print("🔹 Ejecutando caso RF-001: Registro de Instructor")
-        result = RF_001.run(driver)
+        # result = CP_RF001_001.run(driver)
+        # print(result)
+
+        # LOGIN INSTRUCTOR
+        print("🔹 Iniciando login como instructor para probar RF-0021")
+        if not login_como_instructor(driver):
+            return print("⚠️ No se pudo autenticar. Revisa manualmente.")
+
+        print("🔹 Ejecutando caso RF-0021: --")
+        print("CP_RF021_001:")
+        result = CP_RF021_001.run(driver)
+        print(result)
+        print("CP_RF021_002:")
+        result = CP_RF021_002.run(driver)
+        print(result)
+        print("CP_RF021_003:")
+        result = CP_RF021_003.run(driver)
         print(result)
 
     finally:
